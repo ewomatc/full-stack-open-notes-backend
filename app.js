c5e6b827f263d870933407daca5597acfdf77d99
Great Diro <iamgreatdiro@gmail.com>
2023-04-15 16:23:36 +0100

    update app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const express = require('express')
 const app = express()
 
 
-const notes = [
+let notes = [
   {
     id: 1,
     content: "HTML is easy",
@@ -31,6 +31,40 @@ app.get('/api/notes', (req, res) => {
   console.log('requesting notes')
   res.json(notes)
 })
+
+//fetching a single resource
+app.get('/api/notes/:id', (req, res) => {
+  const id = Number(req.params.id)
+
+  const note = notes.find(note => note.id === id )
+//here we check if the requested id exists and send an appropriate response
+  if(note) {
+    res.json(note)
+    console.log(`request made for note: ${note.id}`);
+  } else {
+    res.status(404).end()
+  }
+})
+
+//delete a note
+app.delete('/api/notes/:id', (req, res) => {
+  const id = Number(req.params.id)
+  notes = notes.filter(note => note.id !== id)
+  console.log('Data deleted successfully');
+  res.status(204).end()
+})
+
+
+
+
+
+
+
+
+
+
+
+
 
 const PORT = 3001;
 app.listen(PORT, () => {
