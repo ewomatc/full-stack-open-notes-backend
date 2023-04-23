f860ea894575468f13b28493670e3e8dde9fa6b8
Great Diro <iamgreatdiro@gmail.com>
2023-04-24 00:52:48 +0100

    updated app.js, added a bunch of other files

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const http = require('http')
 const express = require('express')
 
 const app = express()
+app.use(express.json())
 
 
 let notes = [
@@ -54,14 +55,42 @@ app.delete('/api/notes/:id', (req, res) => {
   res.status(204).end()
 })
 
+//add a note with POST
+const generateId = () => {
+  const maxId = notes.length > 0
+    ? Math.max(...notes.map(n => n.id))
+    : 0
+  return maxId + 1
+}
+
+app.post('/api/notes', (request, response) => {
+  const body = request.body
+
+  if (!body.content) {
+    return response.status(400).json({ 
+      error: 'content missing' 
+    })
+  }
+
+  const note = {
+    content: body.content,
+    important: body.important || false,
+    id: generateId(),
+  }
+
+  notes = notes.concat(note)
+
+  response.json(note)
+})
 
 
 
 
-
-
-
-
+//middleware
+const unknownEndpoint = (req, res) => {
+  res.status(404).json({error: 'Unknown Endpoint'})
+}
+app.use(unknownEndpoint)
 
 
 
